07f84abc667ee78025d1799efb742704753356a9
stasvebg <stasveng@gmail.com>
2024-10-05 13:59:58 +0300

    Add project
    Add project

---
+++ Assets/TimeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeScript : MonoBehaviour
+{
+    private int score = 0;
+    IEnumerator DoCheck()
+    {
+        for (; ; )
+        {
+            score++;
+            this.GetComponent<Text>().text = score.ToString();
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    private void Start()
+    {
+        StartCoroutine("DoCheck");
+    }
+
+    private void Update()
+    {
+        
+    }
+}
